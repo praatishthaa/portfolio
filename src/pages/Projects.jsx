b91ec63ef71a46cf9e02e65602f74a707f46b38f
Praatishthaa <praatishthaagoyal126@gmail.com>
2025-07-05 15:20:25 +0530

    Added new projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -17,7 +17,42 @@ const Projects = () => {
           {/* Add more <ProjectCard />s as needed */}
         </div>
       </div>
+
+
+
+      <div className="max-w-6xl mx-auto px-6">
+        <h2 className="text-3xl md:text-4xl font-bold text-center text-gray-800 dark:text-white mb-12">Projects</h2>
+        <div className="grid md:grid-cols-2 gap-10">
+          <ProjectCard
+            title="VaayuFlo AI"
+            description="VaayuFlow AI delivers real-time and predictive air quality insights tailored for rural and small-town India. Built using React and geolocation tech, it visualizes granular AQI data through interactive maps and charts."
+            tech="React, Tailwind CSS, Recharts, Geolocation API, and Leaflet"
+            githubLink="https://github.com/praatishthaa/VaayuFlowAI"
+            liveLink="https://vaayuflow-ai.vercel.app/"
+          />
+          {/* Add more <ProjectCard />s as needed */}
+        </div>
+      </div>
+
+
+       <div className="max-w-6xl mx-auto px-6">
+        <h2 className="text-3xl md:text-4xl font-bold text-center text-gray-800 dark:text-white mb-12">Projects</h2>
+        <div className="grid md:grid-cols-2 gap-10">
+          <ProjectCard
+            title="Nourivo"
+            description="Nourivo is a fitness-for-impact platform that tracks steps via Google Fit and donates a meal for every 10,000 steps. Users earn milestone badges, while NGOs handle real-time donation logistics."
+            tech="React, Tailwind CSS, Google Fit API, Firebase"
+            githubLink="https://github.com/praatishthaa/Nourivo"
+            liveLink="https://nourivo.vercel.app/"
+          />
+          {/* Add more <ProjectCard />s as needed */}
+        </div>
+      </div>
     </section>
+
+    
+
+
   );
 };
 
